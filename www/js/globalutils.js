2b2de742913f75c226b67b93173b7fab679c1786
Fabrizzio Rivera <frivera@nevicad.com>
2020-09-19 15:45:24 -0600

    cendalza limite de credito

--- www/js/globalutils.js
+++ www/js/globalutils.js
@@ -94,7 +94,7 @@ var default_image;
 var SondaServerURL = "";
 
 var currentBranch = "Agosto12020";
-var SondaVersion = "2020.9.13";
+var SondaVersion = "2020.9.19";
 var SondaServerOptions = {
   reconnect: true,
   "max reconnection attempts": 60000
